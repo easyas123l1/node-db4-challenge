7ee8148e46339b8cf6a54310d9e36e433992c1aa
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-12 16:43:59 -0600

    get list of ingredients for single recipe recipe/:id/shoppingList endpoint

--- recipe/recipe-router.js
+++ recipe/recipe-router.js
@@ -4,4 +4,26 @@ const Recipe = require("./recipe-model");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  Recipe.getRecipes()
+    .then(recipes => {
+      res.status(200).json(recipes);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "failed to get recipes" });
+    });
+});
+
+router.get("/:id/shoppingList", (req, res) => {
+  Recipe.getShoppingList(req.params.id)
+    .then(ingredients => {
+      res.status(200).json(ingredients);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "failed to get ingredients for recipe" });
+    });
+});
+
 module.exports = router;
